d9e0239bf05387c97261871bc82704242873185d
valen <valen@debian>
2020-08-19 10:31:47 +0200

    make dashboard template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { SidebarComponent } from './dashboard/shared/sidebar/sidebar.component';
+import { NavbarComponent } from './dashboard/shared/navbar/navbar.component';
+import { ShoesComponent } from './dashboard/pages/edit/shoes/shoes.component';
+import { JacketsComponent } from './dashboard/pages/edit/jackets/jackets.component';
+import { UsersComponent } from './dashboard/pages/edit/users/users.component';
+import { UserProfileComponent } from './dashboard/pages/user-profile/user-profile.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent, SidebarComponent, NavbarComponent, ShoesComponent, JacketsComponent, UsersComponent, UserProfileComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
